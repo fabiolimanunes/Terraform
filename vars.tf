92ffb1d5f8edac6166ca2b52c34e9c265b35dc15
fabiolimanunes <fabio.lima@moneycloud.com.br>
2023-09-01 16:27:23 -0300

    adiciona arquivo variaveis

---
+++ vars.tf
@@ -0,0 +1,23 @@
+variable "client_id" {
+    type = string
+    default = "04fee3fc-e5e3-457d-ad26-d92ef4ce2ced"
+  
+}
+
+variable "client_secret" {
+    type = string
+    default = "e4E8Q~HkHBe2DJPRi9MC7jsO7Ejbj2vLScA9Jaag"
+  
+}
+
+variable "tenant_id" {
+    type = string
+    default = "10f79c55-29a5-4d35-8566-962333c5ef89"
+  
+}
+
+variable "subscription_id" {
+    type = string
+    default = "74ad44b4-3cc2-4138-bafb-99f5424dc0c2"
+  
+}
